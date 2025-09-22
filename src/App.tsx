d0b900c0599520f623f43b18e703ad05f6dc7432
yeondon <s25030@gsm.hs.kr>
2025-09-22 23:39:35 +0900

    fragment 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
