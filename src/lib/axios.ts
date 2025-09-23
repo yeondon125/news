1ec99ed31fb6bee1a6170fa987d343010c9be2e5
yeondon <s25030@gsm.hs.kr>
2025-09-23 20:02:31 +0900

    CORS 문제 해결

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const naverApi = axios.create({
-  baseURL: "https://openapi.naver.com/v1/search/news.json",
+  baseURL: "/api",
   headers: {
     "X-Naver-Client-Id": import.meta.env.VITE_NAVER_CLIENT_ID!,
     "X-Naver-Client-Secret": import.meta.env.VITE_NAVER_CLIENT_SECRET!,
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://openapi.naver.com/v1/search/news.json",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
